e391191942cd369b83beadce668b93ff48dc8d01
gy0801151351 <gy0801151351@qq.com>
2018-04-13 11:07:51 +0800

    11

--- boot-parent/mvc-netty/src/main/java/cc/teddy/configurator/BasicConfigurator.java
+++ boot-parent/mvc-netty/src/main/java/cc/teddy/configurator/BasicConfigurator.java
@@ -9,10 +9,16 @@ import reactor.core.scheduler.Schedulers;
 @Configuration
 public class BasicConfigurator {
 
+
 	@Bean(name = "controllerMethodScheduler")
 	Scheduler controllerMethodScheduler() {
-		Scheduler scheduler = Schedulers.newParallel("controller-parallel-thread", 120, true);
-//		Scheduler scheduler = Schedulers.newElastic("controller-elastic-thread", 200, true);
+		int threadCount = 400;
+//		Scheduler scheduler = Schedulers.newParallel("controller-parallel-thread");
+		Scheduler scheduler = Schedulers.newParallel("controller-parallel-thread", threadCount, true);
+//		Scheduler scheduler = Schedulers.newElastic("controller-elastic-thread", 10, true);
+		for (int i = 0; i < threadCount; i++) {
+			scheduler.schedule(() -> {});
+		}
 		return scheduler;
 	}
 }
